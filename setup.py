7f97ddd84550d6cf0e383eb4aa4876244401da63
Your Name <your_email@example.com>
2024-10-17 21:10:14 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+
+
+
+
+
+
+
+
+setup(
+name= 'mlproject',
+version= '0.0.1',
+author='Shivani',
+author_email='shivaninaik1512@gmail.com',
+packages=find_packages(),
+install_requires= ['numpy','pandas']
+ 
+ 
+)
